571a34e9f637a8109382c206a0465da8ddd7c581
GroguA <117662071+GroguA@users.noreply.github.com>
2024-08-30 14:39:09 +1200

    Create view module

--- ToDoListMainScreen/View/ToDoListCollectionViewCell.swift
+++ ToDoListMainScreen/View/ToDoListCollectionViewCell.swift
@@ -8,5 +8,73 @@
 import UIKit
 
 class ToDoListCollectionViewCell: UICollectionViewCell {
+    static let identifier = String(describing: ToDoListCollectionViewCell.self)
     
+    private lazy var taskTitleLabel = LabelsFactory.createLabel(isTextBold: true)
+    private lazy var taskTextLabel = LabelsFactory.createLabel(textSize: 18)
+    private lazy var taskCreationDateLabel = LabelsFactory.createLabel(textSize: 18)
+    private lazy var taskStatusLabel = LabelsFactory.createLabel(textSize: 18)
+    
+    private let offsetForConstraints: CGFloat = 16
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        contentView.addSubview(taskTitleLabel)
+        contentView.addSubview(taskTextLabel)
+        contentView.addSubview(taskCreationDateLabel)
+        contentView.addSubview(taskStatusLabel)
+        
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.gray.withAlphaComponent(0.2).cgColor
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+        
+        setupConstraints()
+    }
+    
+    func fillCellWith(_ task: TaskModel) {
+        taskTextLabel.text = task.description
+        taskStatusLabel.text = task.status ? "Completed" : "Not completed"
+        taskCreationDateLabel.text = task.creationDate
+        guard let title = task.title else {
+            taskTitleLabel.text = "Title"
+            return
+        }
+        taskTitleLabel.text = title
+    }
+}
+
+private extension ToDoListCollectionViewCell {
+    func setupConstraints() {
+        let constraints = [
+            taskTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: offsetForConstraints),
+            taskTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: offsetForConstraints),
+            taskTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -offsetForConstraints),
+            
+            taskTextLabel.topAnchor.constraint(equalTo: taskTitleLabel.bottomAnchor, constant: offsetForConstraints),
+            taskTextLabel.leadingAnchor.constraint(equalTo: taskTitleLabel.leadingAnchor),
+            taskTextLabel.trailingAnchor.constraint(equalTo: taskTitleLabel.trailingAnchor),
+            
+            taskCreationDateLabel.topAnchor.constraint(equalTo: taskTextLabel.bottomAnchor, constant: offsetForConstraints),
+            taskCreationDateLabel.leadingAnchor.constraint(equalTo: taskTitleLabel.leadingAnchor),
+            taskCreationDateLabel.trailingAnchor.constraint(equalTo: taskTitleLabel.trailingAnchor),
+            
+            taskStatusLabel.topAnchor.constraint(equalTo: taskCreationDateLabel.bottomAnchor, constant: offsetForConstraints),
+            taskStatusLabel.leadingAnchor.constraint(equalTo: taskTitleLabel.leadingAnchor),
+            taskStatusLabel.trailingAnchor.constraint(equalTo: taskTitleLabel.trailingAnchor),
+            taskStatusLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -offsetForConstraints),
+            
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
 }
--- ToDoListMainScreen/View/ToDoListContainerView.swift
+++ ToDoListMainScreen/View/ToDoListContainerView.swift
@@ -7,14 +7,95 @@
 
 import UIKit
 
-class ToDoListContainerView: UIView {
+final class ToDoListContainerView: UIView {
+    lazy var taskCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = dataSource
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(ToDoListCollectionViewCell.self, forCellWithReuseIdentifier: ToDoListCollectionViewCell.identifier)
+        return collectionView
+    }()
+    
+    private lazy var layout: UICollectionViewCompositionalLayout = {
+        let layout = UICollectionViewCompositionalLayout() { section, layoutEnvironment in
+            var config = UICollectionLayoutListConfiguration(appearance: .plain)
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+            config.trailingSwipeActionsConfigurationProvider = { indexPath in
+                let deleteAction = UIContextualAction(style: .destructive, title: "Delete") {
+                    [weak self] action, view, completion in
+                    guard let self else { return }
+                    self.deleteActionClosure(self.dataSource.tasks[indexPath.row].id)
+                    completion(true)
+                }
+                return UISwipeActionsConfiguration(actions: [deleteAction])
+            }
+
+            config.leadingSwipeActionsConfigurationProvider = { indexPath in
+                let completeAction = UIContextualAction(style: .normal, title: "Done") {
+                    [weak self] action, view, completion in
+                    guard let self else { return }
+                    self.completeActionClosure(self.dataSource.tasks[indexPath.row].id)
+                    completion(true)
+                }
+                completeAction.backgroundColor = .systemGreen
+                return UISwipeActionsConfiguration(actions: [completeAction])
+            }
+
+            let layoutSection = NSCollectionLayoutSection.list(using: config, layoutEnvironment: layoutEnvironment)
+            layoutSection.interGroupSpacing = 8
+            return layoutSection
+        }
+        return layout
+    }()
+
+    
+    lazy var addTaskButton: UIImageView = {
+        let button = UIImageView()
+        button.image = UIImage(systemName: "plus.circle.fill")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isUserInteractionEnabled = true
+        return button
+    }()
+    
+    let dataSource = ToDoListMainCollectionViewDataSource()
+    
+    var deleteActionClosure: ((Int) -> Void) = { _ in }
+    var completeActionClosure: ((Int) -> Void) = { _ in }
+    
+    init(delegate: UICollectionViewDelegate) {
+        super.init(frame: .zero)
+        taskCollectionView.delegate = delegate
+        setupViews()
     }
-    */
-
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
+
+private extension ToDoListContainerView {
+    func setupViews() {
+        backgroundColor = .systemBackground
+        addSubview(taskCollectionView)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        let constraint = [
+            taskCollectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            taskCollectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            taskCollectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            taskCollectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            
+            addTaskButton.widthAnchor.constraint(equalToConstant: 34),
+            addTaskButton.heightAnchor.constraint(equalToConstant: 34),
+        ]
+        
+        NSLayoutConstraint.activate(constraint)
+    }
+}
+
+
