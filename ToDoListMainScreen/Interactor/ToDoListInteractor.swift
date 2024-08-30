db7904f899cfe87c77821f76eaaff004430addac
GroguA <117662071+GroguA@users.noreply.github.com>
2024-08-30 14:39:49 +1200

    Create presenter and interactor module

--- ToDoListMainScreen/Interactor/ToDoListInteractor.swift
+++ ToDoListMainScreen/Interactor/ToDoListInteractor.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+protocol IToDoListInteractor {
+    func fetchPoints(completion: @escaping (Result<[TaskModel], Error>) -> Void)
+}
+
+final class ToDoListInteractor {
+    private let networkService = TasksNetworkService()
+    private var currentTasks = [TaskModel]()
+}
+
+extension ToDoListInteractor: IToDoListInteractor {
+    func fetchPoints(completion: @escaping (Result<[TaskModel], Error>) -> Void) {
+        networkService.getDefaultTasks { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let tasks):
+                let mappedTasks = self.mapTasksSchemeToMoviesModel(tasks)
+                self.currentTasks = mappedTasks
+                completion(.success(mappedTasks))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+private extension ToDoListInteractor {
+    func mapTasksSchemeToMoviesModel(_ tasks: [TaskScheme]) -> [TaskModel] {
+        var mappedTasks = [TaskModel]()
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        tasks.forEach { taskScheme in
+            let mappedTask = TaskModel(
+                title: nil,
+                description: taskScheme.todo,
+                creationDate: dateFormatter.string(from: currentDate), 
+                id: taskScheme.id,
+                status: taskScheme.completed
+            )
+            mappedTasks.append(mappedTask)
+        }
+        return mappedTasks
+    }
+}
--- ToDoListMainScreen/Presenter/ToDoListPresenter.swift
+++ ToDoListMainScreen/Presenter/ToDoListPresenter.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+protocol IToDoListPresenter {
+    func didLoad(ui: IToDoListController)
+}
+
+final class ToDoListPresenter {
+    private weak var ui: IToDoListController?
+    private var tasks = [TaskModel]()
+    private let interactor: IToDoListInteractor
+    private let router: IToDoListRouter
+    
+    init(interactor: IToDoListInteractor, router: IToDoListRouter) {
+        self.interactor = interactor
+        self.router = router
+    }
+}
+
+
+extension ToDoListPresenter: IToDoListPresenter {
+    func didLoad(ui: IToDoListController) {
+        self.ui = ui
+        interactor.fetchPoints { [weak self] result in
+            switch result {
+            case .success(let tasks):
+                self?.tasks = tasks
+                self?.ui?.showTasks(tasks)
+            case .failure(_): break
+                
+            }
+        }
+    }
+}
+
