2dcd62a1d2ba83d65bd8488ccb265ac63c890a1b
GroguA <117662071+GroguA@users.noreply.github.com>
2024-09-03 16:25:30 +1200

    Add persistent storage layer

--- Services/PersistentStorageLayer/TaskStorageService.swift
+++ Services/PersistentStorageLayer/TaskStorageService.swift
@@ -10,102 +10,40 @@ import CoreData
 
 protocol ITaskStorageService {
     func fetchTasks() throws -> [TaskStorageModel]
-    func createTask(title: String, text: String?) throws
-    func getTaskById(_ id: UUID) throws -> TaskStorageModel?
-    func updateTaskById(id: UUID, title: String?, text: String?) throws
+    func createTask(title: String?, text: String?) throws -> String
+    func getTaskById(_ id: String) throws -> TaskStorageModel?
+    func updateTaskById(_ id: String, title: String?, text: String?) throws
 }
 
 final class TaskStorageService {
     static let shared = TaskStorageService()
     
-    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    private let persistentContainer: NSPersistentContainer
     
-    private init() {}
+    private init(persistentContainer: NSPersistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer) {
+        self.persistentContainer = persistentContainer
+    }
     
+    private var managedContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
 }
 
 extension TaskStorageService: ITaskStorageService {
     func fetchTasks() throws -> [TaskStorageModel] {
-        var tasks = [TaskStorageModel]()
-        
-        guard let appDelegate = appDelegate else {
-            throw StorageErrors.runtimeError("No app delegate")
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
         
-        let notesManagedObjects = try managedContext.fetch(fetchRequest)
-        notesManagedObjects.forEach({ managedNote in
-            guard let title = managedNote.value(forKey: "title") as? String,
-                  let text = managedNote.value(forKey: "text") as? String,
-                  let creationDate = managedNote.value(forKey: "creationDate") as? Date,
-                  let id = managedNote.value(forKey: "id") as? UUID,
-                  let status = managedNote.value(forKey: "status") as? Bool
-            else {
-                return
-            }
-            let task = TaskStorageModel(
-                title: title,
-                text: text,
-                creationDate: creationDate,
-                id: id,
-                status: status
-            )
-            tasks.append(task)
-        })
-        return tasks
-    }
-    
-    func createTask(title: String, text: String?) throws {
-        guard let appDelegate = appDelegate else {
-            throw StorageErrors.runtimeError("No app delegate")
-        }
-
-        let managedContext = appDelegate.persistentContainer.viewContext
-
-        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: managedContext) else {
-            throw StorageErrors.runtimeError("No entity for task")
-        }
-
-        let taskManagedObject = NSManagedObject(entity: entity, insertInto: managedContext)
-
-        let id = UUID()
-
-        taskManagedObject.setValue(title, forKey: "title")
-        taskManagedObject.setValue(text, forKey: "text")
-        taskManagedObject.setValue(id, forKey: "id")
-        taskManagedObject.setValue(false, forKey: "status")
-
-        do {
-            try managedContext.save()
-        } catch {
-            throw StorageErrors.runtimeError("Could not save task: \(error.localizedDescription)")
-        }
-    }
-
-    
-    func getTaskById(_ id: UUID) throws -> TaskStorageModel? {
-        guard let appDelegate = appDelegate else {
-            throw StorageErrors.runtimeError("No app delegate")
-        }
-
-        let managedContext = appDelegate.persistentContainer.viewContext
-
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
         
-        fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+        fetchRequest.sortDescriptors = [sortDescriptor]
         
-        let fetchedTasks = try managedContext.fetch(fetchRequest)
+        let taskManagedObjs = try managedContext.fetch(fetchRequest)
         
-        if let managedTask = fetchedTasks.first {
+        return taskManagedObjs.compactMap { managedTask in
             guard let title = managedTask.value(forKey: "title") as? String,
                   let text = managedTask.value(forKey: "text") as? String,
                   let creationDate = managedTask.value(forKey: "creationDate") as? Date,
-                  let id = managedTask.value(forKey: "id") as? UUID,
-                  let status = managedTask.value(forKey: "status") as? Bool
-            else {
+                  let status = managedTask.value(forKey: "status") as? Bool else {
                 return nil
             }
             
@@ -113,44 +51,89 @@ extension TaskStorageService: ITaskStorageService {
                 title: title,
                 text: text,
                 creationDate: creationDate,
-                id: id,
+                id: managedTask.objectID.uriRepresentation().absoluteString,
                 status: status
             )
         }
-        return nil
     }
-
-    func updateTaskById(id: UUID, title: String?, text: String?) throws {
-        guard let appDelegate = appDelegate else {
-            throw StorageErrors.runtimeError("No app delegate")
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
-        fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
-        
-        do {
-            let fetchedTasks = try managedContext.fetch(fetchRequest)
-            
-            guard let taskManagedObject = fetchedTasks.first else {
-                throw StorageErrors.runtimeError("Task with ID \(id) not found")
-            }
-            
-            if let title {
-                taskManagedObject.setValue(title, forKey: "title")
-            }
-            
-            if let text {
-                taskManagedObject.setValue(text, forKey: "text")
-            }
-            
-            try managedContext.save()
-            
-        } catch {
-            throw StorageErrors.runtimeError("Failed to update task: \(error.localizedDescription)")
-        }
-    }
-
     
+    func createTask(title: String?, text: String?) throws -> String {
+        let entity = try getEntity(named: "Task")
+        let taskManagedObject = NSManagedObject(entity: entity, insertInto: managedContext)
+                
+        taskManagedObject.setValue(title, forKey: "title")
+        taskManagedObject.setValue(text, forKey: "text")
+        taskManagedObject.setValue(false, forKey: "status")
+        taskManagedObject.setValue(Date(), forKey: "creationDate")
+        
+        try saveContext()
+        return taskManagedObject.objectID.uriRepresentation().absoluteString
+    }
+    
+    func getTaskById(_ id: String) throws -> TaskStorageModel? {
+        let taskManagedObj = try getManagedObjectById(id)
+        
+        guard let title = taskManagedObj.value(forKey: "title") as? String?,
+              let text = taskManagedObj.value(forKey: "text") as? String?,
+              let creationDate = taskManagedObj.value(forKey: "creationDate") as? Date,
+              let status = taskManagedObj.value(forKey: "status") as? Bool else {
+            return nil
+        }
+        
+        return TaskStorageModel(
+            title: title,
+            text: text,
+            creationDate: creationDate,
+            id: taskManagedObj.objectID.uriRepresentation().absoluteString,
+            status: status
+        )
+    }
+    
+    func updateTaskById(_ id: String, title: String?, text: String?) throws {
+        let taskManagedObj = try getManagedObjectById(id)
+        
+        guard let title, let text else { return }
+        guard !title.isEmpty || !text.isEmpty else {
+            try deleteTask(id: id)
+            return
+        }
+        
+        taskManagedObj.setValue(title, forKey: "title")
+        taskManagedObj.setValue(text, forKey: "text")
+        taskManagedObj.setValue(Date(), forKey: "creationDate")
+        
+        try saveContext()
+    }
+    
+    func deleteTask(id: String) throws {
+        let taskManagedObj = try getManagedObjectById(id)
+        managedContext.delete(taskManagedObj)
+        try saveContext()
+    }
 }
+
+private extension TaskStorageService {
+    func getEntity(named name: String) throws -> NSEntityDescription {
+        guard let entity = NSEntityDescription.entity(forEntityName: name, in: managedContext) else {
+            throw StorageErrors.runtimeError("No entity for name: \(name)")
+        }
+        return entity
+    }
+    
+    func getManagedObjectById(_ id: String) throws -> NSManagedObject {
+        guard let url = URL(string: id),
+              let taskId = persistentContainer.persistentStoreCoordinator.managedObjectID(forURIRepresentation: url) else {
+            throw StorageErrors.runtimeError("Failed to retrieve object for id: \(id)")
+        }
+        return managedContext.object(with: taskId)
+    }
+    
+    func saveContext() throws {
+        do {
+            try managedContext.save()
+        } catch {
+            throw StorageErrors.runtimeError("Failed to save context: \(error.localizedDescription)")
+        }
+    }
+}
+
