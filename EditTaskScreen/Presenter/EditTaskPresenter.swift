234c896038c76457c5daba3ff53449e479d3f537
GroguA <117662071+GroguA@users.noreply.github.com>
2024-09-03 16:23:46 +1200

    Add presenter and interactor module to edit task screen

--- EditTaskScreen/Presenter/EditTaskPresenter.swift
+++ EditTaskScreen/Presenter/EditTaskPresenter.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+protocol IEditTaskPresenter {
+    func didLoad(ui: IEditTaskViewController)
+    func taskChanged(title: String?, text: String?)
+}
+
+final class EditTaskPresenter {
+    private weak var ui: IEditTaskViewController?
+    
+    private let interactor: IEditTaskInteractor
+    
+    init(interactor: IEditTaskInteractor) {
+        self.interactor = interactor
+    }
+}
+
+extension EditTaskPresenter: IEditTaskPresenter {
+    func didLoad(ui: IEditTaskViewController) {
+        self.ui = ui
+        interactor.getTask() { [weak self] result in
+            switch result {
+            case .success(let task):
+                self?.ui?.showTask(task)
+            case .failure(let failure):
+                self?.ui?.showError(failure.localizedDescription)
+            }
+        }
+    }
+    
+    func taskChanged(title: String?, text: String?) {
+        interactor.updateTask(title: title, text: text) { [weak self] result in
+            switch result {
+            case .success(_):
+                return
+            case .failure(let failure):
+                self?.ui?.showError(failure.localizedDescription)
+            }
+        }
+    }
+
+}
